f52cb95980826e41615ea1b098475eae36d4244f
Iman Majdabadi <imanclever51@gmail.com>
2022-12-21 17:51:38 +0330

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React, { FC } from 'react'
-import BearApp from './components/BearApp/App'
-import ImanApps from './components/Main/ImanApps'
+
 import TodoApp from './TodoApp'
 
 
 
-const App:FC = () => {
+const App: FC = () => {
   return (
-    <div>
-      <TodoApp/>
-    </div>
+    <TodoApp />
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React, { createContext, useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,19 +7,6 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-
   <App />
-
 );
 
-// interface User {
-//   id?: string;
-//   name?: string;
-// }
-
-// const UserContext = createContext<User>({
-//   id: undefined,
-//   name: undefined
-// })
-
-// const user = useContext(UserContext);
